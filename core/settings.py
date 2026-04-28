c43e89aca14acc70974a6e45bbf555f343e5db64
Leonal Robin D <leonal2003lr10@gmail.com>
2026-04-28 22:52:00 +0530

    feat: initialize project settings with Django, DRF, and Celery configurations

--- core/settings.py
+++ core/settings.py
@@ -94,6 +94,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+CORS_ALLOW_ALL_ORIGINS = config("CORS_ALLOW_ALL_ORIGINS", default=False, cast=bool)
 CORS_ALLOWED_ORIGINS = config(
     "CORS_ALLOWED_ORIGINS",
     default="http://localhost:3000,http://127.0.0.1:3000",
